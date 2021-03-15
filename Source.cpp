46236ae148253029fd30352560fd07f3e0e79088
mattyj4457 <73541833+mattyj4457@users.noreply.github.com>
2021-03-15 12:32:11 -0400

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "player.h"
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    
+}
---
+++ player.cpp
@@ -0,0 +1,15 @@
+#include "player.h"
+
+player::player()
+{
+}
+
+player::~player()
+{
+}
+
+string player::Hits()
+{
+	return string();
+}
+
